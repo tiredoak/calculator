f03ee5189d69db5c4eae56b04d1582c2dea53ed5
Miguel <miguel@Miguels-MacBook-Pro.local>
2020-06-06 19:50:38 +0100

    One static button, add CalculatorButton component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CalculatorButton from "./components/CalculatorButton";
 
-function App() {
+const App = () => {
+  const [expression, setExpression] = useState("");
+  let value = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Test</div>
+      <button>
+        <CalculatorButton value={value}></CalculatorButton>
+      </button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/CalculatorButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalculatorButton = (props) => {
+  return <p>{props.value}</p>;
+};
+
+export default CalculatorButton;
